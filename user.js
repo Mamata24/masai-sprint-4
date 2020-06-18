357e8a5b28879fb5346900a3ee9d885951681d80
Mamata24 <nmamta240@gmail.com>
2020-06-18 16:46:49 +0530

    BACKEND- updated user page

--- user.js
+++ user.js
@@ -5,35 +5,45 @@
 var detailsbtn = document.getElementById("detailsbtn");
 var bookingbtn = document.getElementById("bookingbtn");
 
-detailsbtn.addEventListener('click', toggle)
+detailsbtn.addEventListener('click', toggle1)
 
-bookingbtn.addEventListener('click', toggle1)
+bookingbtn.addEventListener('click', toggle2)
 
-function toggle() {
-    console.log("toggle1")
-    var st = document.querySelector('.details')
-    var st2 = document.querySelector('.book')
-    st.classList.toggle("needs-validation")
-    st2.classList.toggle("needs-validation")
-
-    console.log(st)
-    // if (st.classList.contains('needs-validation')) {
-    //     document.querySelector('.book').classList.remove('needs-validation');
-
-    // }
-    // else {
-    //     document.querySelector('.show').classList.remove('needs-validation');
-
-    // }
-}
 function toggle1() {
-    console.log("toggle2")
+    console.log("toggle1")
     var st = document.querySelector('.book')
-    var st1 = document.querySelector('.details')
-    st.classList.toggle("needs-validation")
-    st1.classList.toggle("needs-validation")
-    console.log(st)
-}
-var venue = document.getElementById("venue").addEventListener('change', function () {
+    if (st.classList.contains('needs-validation')) {
+        document.querySelector('.details').classList.remove('needs-validation');
+    }
+    else {
+        st.classList.add('needs-validation')
+        document.querySelector('.details').classList.remove('needs-validation');
 
-})
+    }
+}
+function toggle2() {
+    console.log("toggle2")
+    var st = document.querySelector('.details')
+    if (st.classList.contains('needs-validation')) {
+        document.querySelector('.book').classList.remove('needs-validation');
+    }
+    else {
+        st.classList.add('needs-validation')
+        document.querySelector('.book').classList.remove('needs-validation');
+
+    }
+
+
+
+
+
+
+    // var st = document.querySelector('.book')
+    // var st1 = document.querySelector('.details')
+    // st.classList.toggle("needs-validation")
+    // st1.classList.toggle("needs-validation")
+    // console.log(st)
+}
+// var venue = document.getElementById("venue").addEventListener('change', function () {
+
+// })
