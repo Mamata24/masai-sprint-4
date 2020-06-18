8e2796e3adb85f497cfc4a1cae7a3c7e45b509ff
Mamata24 <nmamta240@gmail.com>
2020-06-18 13:15:14 +0530

    BACKEND- User Login created

---
+++ userLogin.js
@@ -0,0 +1,66 @@
+//login form
+document.getElementById('login').addEventListener('click', function (event) {
+    event.preventDefault()
+    var username, password, user
+    username = document.getElementById('username')
+    password = document.getElementById('password')
+    user = JSON.parse(localStorage.getItem([username.value]))
+    if (user) {
+        console.log(user.password, password.value)
+        if (user["password"] === password.value) {
+            alert("Login Successful")
+            window.localStorage.setItem('user', username.value)
+            window.location.href = './user.html'
+        }
+        else {
+            alert('Incorrect Password')
+        }
+    }
+})
+document.getElementById('register').addEventListener('click', function (event) {
+    event.preventDefault()
+    var email, username, password, cnfpassword, arr,
+        email = document.getElementById('email')
+    username = document.getElementById('username')
+    password = document.getElementById('password')
+    cnfpassword = document.getElementById('cnfpassword')
+    if (localStorage.getItem([username.value])) {
+        alert('Username already exist')
+    }
+    else if (password.value === cnfpassword.value) {
+        arr = {
+            email: email.value,
+            username: username.value,
+            password: password.value,
+        };
+        arr = JSON.stringify(arr)
+        localStorage.setItem([username.value], arr)
+        clearfields(email, username, password, cnfpassword)
+        alert("New user Created")
+    }
+    else {
+        alert("Password and confirm password doesnt match")
+    }
+})
+function clearfields(email, username, password, cnfpassword, mob, address) {
+    email.value = ''
+    username.value = ''
+    password.value = ''
+    cnfpassword.value = ''
+    mob.value = ''
+    address.value = ''
+}
+
+var registerbtn = document.getElementById('registerBtn');
+var loginBtn = document.getElementById('loginBtn')
+
+loginBtn.addEventListener('click', toggle)
+
+registerbtn.addEventListener('click', toggle)
+
+function toggle() {
+    var register = document.querySelector('.register-form');
+    var login = document.querySelector('.login-form')
+    register.classList.toggle('show')
+    login.classList.toggle('show')
+}
